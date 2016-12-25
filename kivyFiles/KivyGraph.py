1bcfb08a8b031b32a46501978a15741fe89ac582
talkami <talkaminker@yahoo.com>
2016-12-25 17:12:53 +0200

    documentation added. function 'move_random' renamed to 'centralize_random_node'.

--- kivyFiles/KivyGraph.py
+++ kivyFiles/KivyGraph.py
@@ -10,18 +10,27 @@ class KivyGraph(Widget):
     nodes = []
     edges = []
 
-    def __init__(self,center, **kwargs):
+    def __init__(self, center, **kwargs):
         super(KivyGraph, self).__init__(**kwargs)
         self.center_coor = center
 
-
     def add_node(self,node):
+        """
+        function adds a given node to graph's nodes list
+        """
         self.nodes.append(node)
 
     def add_edge(self,edge):
+        """
+        function adds a given edge to graph's list of edges
+        """
         self.edges.append(edge)
 
     def get_by_serial(self, serial):
+        """
+        :param serial:
+        :return: a node from graph's nodes list associated with given serial
+        """
         for node in self.nodes:
             if node.serial == serial:
                 return node
@@ -53,6 +62,9 @@ class KivyGraph(Widget):
             edge.reset_edge()
 
     def jump(self):
+        """
+        function moves the coordinates of all the nodes in the graph by a random difference
+        """
         x = randint(-100, 100)
         y = randint(-100, 100)
         for node in self.nodes:
@@ -60,11 +72,18 @@ class KivyGraph(Widget):
         for edge in self.edges:
             edge.reset_edge()
 
-    def move_random(self):
+    def centralize_random_node(self):
+        """
+        function chooses a random node and moves it to center of screen using the function "move_node_to_center"
+        """
         i = randint(0,len(self.nodes)-1)
         self.move_node_to_center(self.nodes[i])
 
     def move_node_to_center(self, node):
+        """
+        function moves the graph so that a given node's coordinates are now 'center_coor'
+        :param node: a node to be moved to the center of the screen
+        """
         delta_x = self.center_coor[0] - node.get_x()
         delta_y = self.center_coor[1] - node.get_y()
         for node in self.nodes:
@@ -72,8 +91,14 @@ class KivyGraph(Widget):
         for edge in self.edges:
             edge.reset_edge()
 
+    ### if we will need to find the closest/farthest neighbor often,
+    ### we should consider adding them as KivyNode variables instead of finding them each time using the following two functions.
 
     def get_closest_physical_connection(self,node):
+        """
+        :param node: KivyNode
+        :return: a KivyNode that is connected to node, and has the smallest distance from it.
+        """
         min_dist = -1
         closest_node = None
         for item in node.neighbors:
@@ -86,6 +111,10 @@ class KivyGraph(Widget):
 
 
     def get_farthest_physical_connection(self,node):
+        """
+        :param node: KivyNode
+        :return: a KivyNode that is connected to node, and has the largest distance from it.
+        """
         max_dist = 0
         farthest_node = None
         for item in node.neighbors:
