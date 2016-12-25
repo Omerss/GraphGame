581b2990fea4ecae1b360a604954f4f6ab0ddc16
talkami <talkaminker@yahoo.com>
2016-12-25 12:30:45 +0200

    added variable 'center_coor' that reflects the coordinates of the center of the screen. new functions: **'move_node_to_center' - recieves a node and moves graph so that given node's coordinates are now 'center_coor'. **'move_random' - chooses a random node from graph and moves it to center of screen (using function 'move_node_to_center'). **'get_closest_physical_connection' - recieves a node and returns its closest neighbor. **'get_farthest_physical_connection' - recieves a node and returns its farthest neighbor.

--- kivyFiles/KivyGraph.py
+++ kivyFiles/KivyGraph.py
@@ -1,19 +1,18 @@
 import kivy
 kivy.require('1.9.1')
 
-from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.widget import Widget
-from KivyNode import *
 from random import randint
-from KivyEdge import *
 
 
 class KivyGraph(Widget):
+    center_coor = (0,0)
     nodes = []
     edges = []
 
-    def __init__(self, **kwargs):
+    def __init__(self,center, **kwargs):
         super(KivyGraph, self).__init__(**kwargs)
+        self.center_coor = center
 
 
     def add_node(self,node):
@@ -61,3 +60,38 @@ class KivyGraph(Widget):
         for edge in self.edges:
             edge.reset_edge()
 
+    def move_random(self):
+        i = randint(0,len(self.nodes)-1)
+        self.move_node_to_center(self.nodes[i])
+
+    def move_node_to_center(self, node):
+        delta_x = self.center_coor[0] - node.get_x()
+        delta_y = self.center_coor[1] - node.get_y()
+        for node in self.nodes:
+            node.move_graph(delta_x,delta_y)
+        for edge in self.edges:
+            edge.reset_edge()
+
+
+    def get_closest_physical_connection(self,node):
+        min_dist = -1
+        closest_node = None
+        for item in node.neighbors:
+            other_node = self.get_by_serial(item)
+            curr_dist = node.get_distance_from_node(other_node)
+            if ((curr_dist < min_dist) and (curr_dist != -1)) or (min_dist==-1):
+                min_dist = curr_dist
+                closest_node = other_node
+        return closest_node
+
+
+    def get_farthest_physical_connection(self,node):
+        max_dist = 0
+        farthest_node = None
+        for item in node.neighbors:
+            other_node = self.get_by_serial(item)
+            curr_dist = node.get_distance_from_node(other_node)
+            if (curr_dist > max_dist) and (curr_dist != -1):
+                max_dist = curr_dist
+                farthest_node = other_node
+        return farthest_node
