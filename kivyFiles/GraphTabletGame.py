303a9249e8e3d4751e224c8fd5267df787c8a66f
talkami <talkaminker@yahoo.com>
2017-03-25 12:52:09 +0300

    new functions added! 'get_onscreen_nodes': returns a list of the nodes that can be seen onscreen. 'is_node_onscreen': given a node's serial number and a list of the nodes onscreen, this function checks whether or not the requested node is onscreen. 'get_onscreen_edges': returns a list of tuples, each tuple contain two nodes representing a visible part of an edge. ‘get_partly_visible_edge’: given an edge that is partly visible onscreen, the function returns a tuple of two NodeObjects representing the edge’s nodes. 'get_info_from_screen': function returns a dictionary containing two items; a list of the nodes displayed onscreen and a list of the edges displayed onscreen. This function uses the functions 'get_onscreen_nodes' and 'get_onscreen_edges'.

--- kivyFiles/GraphTabletGame.py
+++ kivyFiles/GraphTabletGame.py
@@ -1,12 +1,14 @@
-import time
-
 from GraphLayout import GraphLayout
 from kivy.app import App
+from Point import Point
+from LineEquation import LineEquation
+from NodeObject import NodeObject
 
 class GraphTabletGame(App):
 
     def build(self):
         self.layout = GraphLayout()
+        self.real_graph = self.layout.original_graph
         return self.layout
 
     def press_button(self,num):
@@ -19,8 +21,150 @@ class GraphTabletGame(App):
         elif (num == 4):
             self.layout.button4_func
 
-    def read_data_from_window(self):
-        pass
+    def get_info_from_screen(self):
+        '''
+        Function returns the nodes and edges that are at least partially displayed onscreen
+        :return: returns a dictionary containing two objects:
+        'nodes': A list containing the nodes that are at least partially displayed onscreen.
+        'edges': A list representing the edges that are at least partially displayed onscreen. Each edge is represented
+                 by a tuple containing of the edge's nodes. If one of the nodes is not onscreen, a new NodeObject is
+                 created where the x,y coordinates represent the intersection between the edge and the screen and the
+                 serial and size are set to None.
+        '''
+        nodes = self.get_onscreen_nodes
+        edges = self.get_onscreen_edges(nodes)
+        return {'nodes':nodes, 'edges':edges}
+
+    def get_onscreen_nodes(self):
+        '''
+        Function goes over the list of nodes in the graph and checks which ones are displayed onscreen
+        :return: A list containing the nodes that are at least partially displayed onscreen.
+        '''
         screen_edges = self.layout.dim
         displayed_nodes = []
+        for node in self.layout.kivy_graph.nodes:
+            node_x = node.get_x()
+            node_y = node.get_y()
+            node_r = node.get_radius()
+            if (node_x + node_r) > screen_edges['min_x'] or (node_x - node_r) < screen_edges['max_x'] or \
+               (node_y + node_r) > screen_edges['min_y'] or (node_y - node_r) < screen_edges['max_x']:
+                real_node = self.real_graph.get_node_by_serial(node.serial)
+                displayed_nodes.append(real_node)
+        return displayed_nodes
+
+    def get_onscreen_edges(self, displayed_nodes):
+        '''
+        Function goes over the list of edges in the graph and checks which ones are displayed onscreen
+        :param displayed_nodes: a list of the nodes displayed onscreen
+        :return: A list representing the edges that are at least partially displayed onscreen. Each edge is represented
+                 by a tuple containing of the edge's nodes. If one of the nodes is not onscreen, a new NodeObject is
+                 created where the x,y coordinates represent the intersection between the edge and the screen and the
+                 serial and size are set to None.
+        '''
+
+        screen_edges = self.layout.dim
+        top_left = Point(screen_edges['min_x'], screen_edges['max_y'])
+        top_right = Point(screen_edges['max_x'], screen_edges['max_y'])
+        bottom_left = Point(screen_edges['min_x'], screen_edges['min_y'])
+        bottom_right = Point(screen_edges['max_x'], screen_edges['min_y'])
+        top = LineEquation.create_equation(top_left,top_right)
+        bottom = LineEquation.create_equation(bottom_left, bottom_right)
+        left = LineEquation.create_equation(bottom_left,top_left)
+        right = LineEquation.create_equation(bottom_right,top_right)
+
+        displayed_edges = []
+
+        for edge in self.layout.kivy_graph.edges():
+            if self.is_node_onscreen(edge.node1.serial, displayed_nodes):
+                if self.is_node_onscreen(edge.node2.serial, displayed_nodes):
+                    first_node = self.real_graph.get_node_by_serial(edge.node1.serial)
+                    second_node = self.real_graph.get_node_by_serial(edge.node2.serial)
+                    curr_edge = (first_node,second_node)
+                    displayed_edges.append(curr_edge)
+                else:
+                    curr_edge = self.get_partly_visible_edge(edge, top, bottom, left, right, edge.node1)
+                    displayed_edges.append(curr_edge)
+            elif self.is_node_onscreen(edge.node2.serial, displayed_nodes):
+                curr_edge = self.get_partly_visible_edge(edge, top, bottom, left, right, edge.node2)
+                displayed_edges.append(curr_edge)
+            else:
+                curr_edge = self.get_partly_visible_edge(edge, top, bottom, left, right, None)
+                displayed_edges.append(curr_edge)
+
+        return displayed_edges
+
+    def is_node_onscreen(self,serial,displayed_nodes):
+        for node in displayed_nodes:
+            if node.serial_num == serial:
+                return True
+
+        return False
+
+    def get_partly_visible_edge(self,edge,top,bottom,left,right,node):
+        '''
+
+        :param edge: an edge that can be seen onscreen but where at least one node is not visible
+        :param top: equation representing the top border of the screen
+        :param bottom: equation representing the bottom border of the screen
+        :param left: equation representing the left border of the screen
+        :param right: equation representing the right border of the screen
+        :param node: the visible node connected to the edge, or None if no node is visible
+        :return: A tuple of two NodeObjects, each representing a one of the edge's nodes. If one of the nodes is not
+        onscreen, the x,y coordinates represent the intersection between the edge and the screen and the serial and
+        size are set to None.
+        '''
+        point1 = Point(edge.node1.get_x(), edge.node1.get_y())
+        point2 = Point(edge.node2.get_x(), edge.node2.get_y())
+        edge_equation = LineEquation.create_equation(point1, point2)
+        first_node = None
+        second_node = None
+
+        if node:
+            first_node = self.real_graph.get_node_by_serial(node.serial)
+
+        # check if edge collides with top border
+        if LineEquation.check_collision_point(edge_equation, top):
+            col_point = LineEquation.get_equation_collision_point(edge_equation, top)
+            if first_node is None:
+                second_node = NodeObject(None,{'x':col_point.x, 'y':col_point.y},None)
+                second_node.real = False
+            else:
+                first_node = NodeObject(None, {'x': col_point.x, 'y': col_point.y}, None)
+                first_node.real = False
+
+        # check if edge collides with bottom border
+        elif LineEquation.check_collision_point(edge_equation, bottom):
+            col_point = LineEquation.get_equation_collision_point(edge_equation, bottom)
+            if first_node is None:
+                second_node = NodeObject(None,{'x':col_point.x, 'y':col_point.y},None)
+                second_node.real = False
+            else:
+                first_node = NodeObject(None, {'x': col_point.x, 'y': col_point.y}, None)
+                first_node.real = False
+
+        # check if edge collides with left border
+        elif LineEquation.check_collision_point(edge_equation, left):
+            col_point = LineEquation.get_equation_collision_point(edge_equation, left)
+            if first_node is None:
+                second_node = NodeObject(None,{'x':col_point.x, 'y':col_point.y},None)
+                second_node.real = False
+            else:
+                first_node = NodeObject(None, {'x': col_point.x, 'y': col_point.y}, None)
+                first_node.real = False
+
+        # check if edge collides with right border
+        elif LineEquation.check_collision_point(edge_equation, right):
+            col_point = LineEquation.get_equation_collision_point(edge_equation, right)
+            if first_node is None:
+                second_node = NodeObject(None,{'x':col_point.x, 'y':col_point.y},None)
+                second_node.real = False
+            else:
+                first_node = NodeObject(None, {'x': col_point.x, 'y': col_point.y}, None)
+                first_node.real = False
+
+        if second_node is None:
+            raise Exception("Did not find two viable nodes for onscreen edge!")
+
+        curr_edge = (first_node, second_node)
+        return curr_edge
 
