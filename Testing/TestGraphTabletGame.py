0dfd588ed3dec7ebb8f36374280b67a329821f64
talkami <talkaminker@yahoo.com>
2017-03-25 17:58:30 +0300

    tests for ‘KivyFiles/graphTabletGame.py’. Currenty only tests button presses and getting the nodes that are onscreen

---
+++ Testing/TestGraphTabletGame.py
@@ -0,0 +1,40 @@
+from kivyFiles.GraphTabletGame import GraphTabletGame
+import unittest
+
+class testGraphTabletGame(unittest.TestCase):
+
+    def test_buttons(self):
+        game = GraphTabletGame()
+        game.build()
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(1)
+        print "pressed button1 (move_up)"
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(2)
+        print "pressed button2 (move_down)"
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(2)
+        print "pressed button2 (move_down)"
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(3)
+        print "pressed button3 (move_right)"
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(4)
+        print "pressed button4 (move_left)"
+        game.layout.kivy_graph.print_graph_nodes()
+        game.press_button(4)
+        print "pressed button4 (move_left)"
+        game.layout.kivy_graph.print_graph_nodes()
+        print "finished checking buttons"
+
+    def test_screen_visibility(self):
+        game = GraphTabletGame()
+        game.build()
+        nodes = game.get_onscreen_nodes()
+        print nodes
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
