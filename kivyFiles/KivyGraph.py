fe23ced05470440c8d38ab1aabfcc640d92f023c
talkami <talkaminker@yahoo.com>
2017-03-30 20:26:34 +0300

    *new variable: ‘real_size’: the graph’s actual size (not just the size displayed onscreen). *the functions move_up, move_down, move_left, move_right now has the option to receive an amount to move by. *new function: ‘resize_graph’: receives a ‘new_x’ and ‘new_y’ values and changes the size and location of all nodes and edges so that the graphs dimensions are now new_x by new_y.

--- kivyFiles/KivyGraph.py
+++ kivyFiles/KivyGraph.py
@@ -10,12 +10,14 @@ class KivyGraph(Widget):
     nodes = None
     edges = None
     center_node = None
+    real_size = {'max_x': 800, 'max_y': 600}
 
-    def __init__(self, center, **kwargs):
+    def __init__(self, center, size, **kwargs):
         super(KivyGraph, self).__init__(**kwargs)
         self.center_coor = center
         self.nodes = []
         self.edges = []
+        self.real_size = size
 
     def add_node(self,node):
         """
@@ -39,28 +41,27 @@ class KivyGraph(Widget):
                 return node
         raise Exception("Node '{}' was not found in node list. Node list = {}".format(serial, self.nodes))
 
-    def move_up(self):
+    def move_up(self, amount = 40):
         for node in self.nodes:
-            node.move_up()
+            node.move_y(amount)
         for edge in self.edges:
             edge.reset_edge()
 
-
-    def move_down(self):
+    def move_down(self, amount = -40):
         for node in self.nodes:
-            node.move_down()
+            node.move_y(amount)
         for edge in self.edges:
             edge.reset_edge()
 
-    def move_left(self):
+    def move_left(self, amount = -40):
         for node in self.nodes:
-            node.move_left()
+            node.move_x(amount)
         for edge in self.edges:
            edge.reset_edge()
 
-    def move_right(self):
+    def move_right(self, amount = 40):
         for node in self.nodes:
-            node.move_right()
+            node.move_x(amount)
         for edge in self.edges:
             edge.reset_edge()
 
@@ -91,13 +92,13 @@ class KivyGraph(Widget):
         function moves the graph so that a given node's coordinates are now 'center_coor' and sets given node as 'center_node'
         :param node: a node to be moved to the center of the screen
         """
-        self.center_node = node
         delta_x = self.center_coor[0] - node.get_x()
         delta_y = self.center_coor[1] - node.get_y()
         for node in self.nodes:
             node.move_by_amount(delta_x,delta_y)
         for edge in self.edges:
             edge.reset_edge()
+        self.center_node = node
 
     ### if we will need to find the closest/farthest neighbor often,
     ### we should consider adding them as KivyNode variables instead of finding them each time using the following two functions.
@@ -132,3 +133,34 @@ class KivyGraph(Widget):
                 max_dist = curr_dist
                 farthest_node = other_node
         return farthest_node
+
+    def resize_graph(self,new_x,new_y, keep_center = True, new_center = None):
+        '''
+        function sets the size of the graph to be new_x by new_y
+        :param new_x: the new 'x' size of the graph
+        :param new_y: the new 'y' size of the graph
+        :param keep_center: if True, the graph's 'center_node' will remain the center node
+        :param new_center: in the form (center_x,center_y). if given will be set as the graph's center coordinates.
+        '''
+
+        change_in_x = float(new_x)/self.real_size['max_x']
+        change_in_y = float(new_y)/self.real_size['max_y']
+        for node in self.nodes:
+            old_size = node.node_size
+            new_size = max(change_in_x,change_in_y)*old_size
+            node.size = [new_size, new_size]
+            node.node_size = new_size
+            node.relative_move(change_in_x,change_in_y)
+        for edge in self.edges:
+            old_size = edge.line_width
+            new_size = max(change_in_x,change_in_y)*old_size
+            edge.line.width = new_size
+            edge.line_width = new_size
+            edge.reset_edge()
+
+        self.real_size = {'max_x': new_x, 'max_y': new_y}
+
+        if new_center is not None:
+            self.center_coor = new_center
+        if (keep_center) and (self.center_node is not None):
+            self.move_node_to_center(self.center_node)
