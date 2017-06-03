9c93fd542791025d847ac0e02695c96d8f216876
talkami <talkaminker@yahoo.com>
2017-06-03 18:33:12 +0300

    reorganization of 'get_question_values' function

--- Questions/QuestionObj.py
+++ Questions/QuestionObj.py
@@ -1,97 +1,36 @@
 import questionsAnswering
 from SupplementaryFiles.Enums import Colours
 
+
 class QuestionObject():
-    question_string = ""
+    question_string = None
     open_question = False
     user_answer = -1
     question_number = 0
     list_of_possible_answers = None
-    args = []
-    def __init__(self, questsion_string, question_number, *args):
-        self.question_string = questsion_string
-        self.quetion_number = question_number
+    args = None
+
+    def __init__(self, question_string, question_number, *args):
+        self.question_string = question_string
+        self.question_number = question_number
         self.args = args
 
-    def get_question_values (self):
-        if (self.quetion_number == 1):
+    def get_question_values(self):
+        if (self.question_number == 1 or self.question_number == 2 or self.question_number == 5):
+            self.open_question = True
+
+        if (self.question_number == 3 or self.question_number == 4 or self.question_number == 6
+            or self.question_number == 7 or self.question_number == 16 or self.question_number == 17):
             self.open_question = False
+            for color in Colours:
+                self.list_of_possible_answers.append(color)
 
-        if (self.quetion_number == 2):
+        if (self.question_number == 8 or self.question_number == 9 or self.question_number == 10
+            or self.question_number == 11 or self.question_number == 12 or self.question_number == 13
+            or self.question_number == 14 or self.quetion__number == 15):
             self.open_question = False
-
-        if (self.quetion_number == 3):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
-
-        if (self.quetion_number == 4):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
-
-        if (self.quetion_number == 5):
-            self.open_question = False
-
-        if (self.quetion_number == 6):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
-
-        if (self.quetion_number == 7):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
-
-        if (self.quetion_number == 8):
-            self.open_question = True
             self.list_of_possible_answers.append(True)
             self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 9):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 10):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 11):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 12):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 13):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 14):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 15):
-            self.open_question = True
-            self.list_of_possible_answers.append(True)
-            self.list_of_possible_answers.append(False)
-
-        if (self.quetion_number == 16):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
-
-        if (self.quetion_number == 17):
-            self.open_question = True
-            for color in Colours:
-                self.list_of_possible_answers.append (color)
 
     def getQuestionString (self):
         return self.question_string
