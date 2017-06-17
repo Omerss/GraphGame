8a9c8e972a36c25064cf2adb2e5f56ed7b842fe5
nogur9 <nogur9@gmail.com>
2017-06-17 13:12:28 +0300

    question object changed

--- Questions/QuestionObj.py
+++ Questions/QuestionObj.py
@@ -1,33 +1,30 @@
 import questionsAnswering
 from SupplementaryFiles.Enums import Colours
 
-
+#an object used for the questions displaying
 class QuestionObject():
     question_string = None
     open_question = False
     user_answer = -1
-    question_number = 0
+    question_type_number = 0
     list_of_possible_answers = None
     args = None
-
-    def __init__(self, question_string, question_number, *args):
+# question_type_number - 1- open question, question_type_number-2 close question with answers derived from colors, question_type_number-3 yes/no question
+    def __init__(self, question_string, question_type_number):
         self.question_string = question_string
-        self.question_number = question_number
-        self.args = args
+        self.question_type_number = question_type_number
+        self.get_question_values()
 
     def get_question_values(self):
-        if (self.question_number == 1 or self.question_number == 2 or self.question_number == 5):
+        if (self.question_type_number== 1):
             self.open_question = True
 
-        if (self.question_number == 3 or self.question_number == 4 or self.question_number == 6
-            or self.question_number == 7 or self.question_number == 16 or self.question_number == 17):
+        if (self.question_type_number == 2):
             self.open_question = False
             for color in Colours:
                 self.list_of_possible_answers.append(color)
 
-        if (self.question_number == 8 or self.question_number == 9 or self.question_number == 10
-            or self.question_number == 11 or self.question_number == 12 or self.question_number == 13
-            or self.question_number == 14 or self.quetion__number == 15):
+        if (self.question_type_number == 3 ):
             self.open_question = False
             self.list_of_possible_answers.append(True)
             self.list_of_possible_answers.append(False)
--- Questions/testingMain.py
+++ Questions/testingMain.py
@@ -4,11 +4,11 @@ from SupplementaryFiles.Enums import Colours
 from SupplementaryFiles.CreateRandGraph import create_rand_graph
 
 def main():
-    questionOne = QuestionObject("how many red nodes there are?", 1,graph, Colours.red)
-    questionThree = QuestionObject("what is the color that contain the node with the maximun links in the graph?", 3, graph)
-    questionFive = QuestionObject("how many ref nodes do not have links blue nodes?", 5,graph, Colours.red, Colours.blue)
-    questionNine = QuestionObject("does every node at color yellow have link to a node of color red?", 9,graph, Colours.yellow, Colours.red)
-    questionTen = QuestionObject( "is there more red nodes than yellow nodes?", 10 , graph,Colours.red, Colours.yellow)
+    questionOne = QuestionObject("how many red nodes there are?", 1)
+    questionThree = QuestionObject("what is the color that contain the node with the maximun links in the graph?", 2)
+    questionFive = QuestionObject("how many red nodes do not have links blue nodes?", 1)
+    questionNine = QuestionObject("does every node at color yellow have link to a node of color red?", 3)
+    questionTen = QuestionObject( "is there more red nodes than yellow nodes?", 3)
 
     questionList = [questionOne,questionThree,questionFive,questionNine, questionTen]
 
