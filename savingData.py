0aa139518c9fa6734e305e82e380113b2e83fcc1
nogur9 <nogur9@gmail.com>
2017-07-08 17:37:54 +0300

    saving data

--- savingData.py
+++ savingData.py
@@ -1,6 +1,6 @@
 '''
 folder - name user id
-insdie:
+inside:
 rounds 10
 each round :
 real graph xml - graph_round_user id
@@ -8,16 +8,30 @@ user seen graph. (real/user_graph_user id_round)
 user meta data - key presses by order. for each question- question id, user's answer, args. all precentgaes. button fuct 
 
 '''
+import os
+from SupplementaryFiles.SaveGraph import save_graph
+import json
+from Questions import AnswerObject
 
 
-
-def save_game_data ():
+def save_game_data (location, user_id, game_round):
     """
     general function that the game handler will call for.
     if there is no user id folder in the  LOCATION this function will create new one.
     the function pass the round number, and the relevant data for the following saving functions  
     :return: 
     """
+    # create folder if not exist
+    directory = location + str(user_id)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    #raise exception
+    error = save_game_graphs (directory, user_id, game_round, real_graph, user_graph)
+
+    #raise exception
+    error = save_game_metadata(location, user_id, game_round, answers_object_list, node_seen_percentage, user_answers_percentage, user_graph_answer_percentage, button_press_list, button_func_list)
+
+
 
 
 def save_game_graphs (location, user_id, game_round, real_graph, user_graph):
@@ -30,6 +44,8 @@ def save_game_graphs (location, user_id, game_round, real_graph, user_graph):
     :param user_graph: graph object
     :return: none if success, error string if failed 
     """
+    save_graph(real_graph, location+"real_graph_"+str(user_id)+"_"+str(round)+".xml")
+    save_graph(user_graph, location+"user's_graph_"+str(user_id)+"_"+str(round)+".xml")
 
 
 def save_game_metadata(location, user_id, game_round, answers_object_list, node_seen_percentage, user_answers_percentage, user_graph_answer_percentage, button_press_list, button_func_list):
@@ -49,6 +65,13 @@ def save_game_metadata(location, user_id, game_round, answers_object_list, node_
     this function will create a file called - metadata_user id_ game round 
     the file will be in json format
     """
+    dict_answers_object_list= {}
+    for answer_object in answers_object_list:
+        dict_answers_object_list.append(create_dict_for_answer_object(answer_object))
+    the_json_data = create_dict_for_game_metadata(dict_answers_object_list, node_seen_percentage, user_answers_percentage, user_graph_answer_percentage, button_press_list, button_func_list)
+
+    with open(location+"user_metadata"+str(user_id)+"_"+str(round)+".txt", 'w') as outfile:
+        json.dump(the_json_data, outfile)
 
 
 def create_dict_for_answer_object(answers_object):
@@ -57,6 +80,8 @@ def create_dict_for_answer_object(answers_object):
     :param answers_object: an valid answers_object 
     :return: dict with the values: question's id, question args, user's answer
     """
+    answer_dict = {'question id number':answers_object.question_number ,''question arguments': answers_object.args,'user answer': answers_object.user_answer}
+    return answer_dict
 
 
 def create_dict_for_game_metadata(dict_answers_object_list, node_seen_percentage, user_answers_percentage, user_graph_answer_percentage, button_press_list, button_func_list):
@@ -70,3 +95,4 @@ def create_dict_for_game_metadata(dict_answers_object_list, node_seen_percentage
     :param button_func_list: list of the button functionality 
     :return: one big dictionary of everything
     """
+    
