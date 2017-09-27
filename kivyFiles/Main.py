bac46201140e5cd41a13febd146984128f36f650
nogur9 <nogur9@gmail.com>
2017-09-27 11:03:03 +0300

    added a comment on main

--- kivyFiles/Main.py
+++ kivyFiles/Main.py
@@ -8,8 +8,10 @@ from KivyGraphTester import *
 
 if __name__ == "__main__":
     button_presses = []
+    #GraphTabletGame require a signal parameter - thats why the main.py doesn't work
     game = GraphTabletGame(create_rand_graph("{}\..\GraphsData\config.ini".format(os.getcwd())), None,
      button_width=100, dim={"max_x": 800, "max_y": 600})
+
     # game = GraphTabletGame(MyGameLayout.get_graph_obj1(), None, button_width=100, dim={"max_x": 800, "max_y": 600})
     # game  = GraphGameApp(button_presses)
     game.run()
