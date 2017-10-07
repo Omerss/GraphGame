8ee20769aeec81aedebc799ba1419e3755c9c786
talkami <talkaminker@yahoo.com>
2017-10-07 12:19:40 +0300

    fixed problem with multiple fast button presses

--- kivyFiles/GameLayout.py
+++ kivyFiles/GameLayout.py
@@ -3,20 +3,23 @@ kivy.require('1.9.1')
 
 from SupplementaryFiles import Utils
 from kivy.uix.gridlayout import GridLayout
-from GraphButton import MultiButton, UniButton
+from GraphButton import MultiButton, TextButton
 from GraphLayout import GraphLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
+from time import sleep
+from SupplementaryFiles.RepeatedTimer import RepeatedTimer
+from kivy.uix.image import Image
 
 
 class GameLayout(FloatLayout):
+    next_func = None
 
-    def __init__(self, tablet_game= None, zoom_rate=0.7, edge_size=2, **kwargs):
+    def __init__(self, tablet_game=None, zoom_rate=0.7, edge_size=2, **kwargs):
         super(GameLayout, self).__init__(rows=1, cols=2, **kwargs)
         self.tablet_game = tablet_game
+        self.move_counter = 0
         self.dim = {"min_x": 0, "min_y": 0, "max_x": kivy.core.window.Window.size[0],
                     "max_y": kivy.core.window.Window.size[1]}
-        # self.dim = {"min_x": 0, "min_y": 0, "max_x": 800, "max_y": 600}
         self.button_width = self.dim["max_x"] * self.tablet_game.game_screen.button_ratio
         self.buttons = []
         self.original_graph = self.tablet_game.original_graph
@@ -31,6 +34,9 @@ class GameLayout(FloatLayout):
         self.button_layout = self.get_buttons()
         self.add_widget(self.button_layout)
         self.button_layout.pos=(0,0)
+        self.rt = RepeatedTimer(1, self.call_function)
+        self.button_hider = self.get_button_hider()
+        self.button_hider.pos=(0,0)
 
     def set_button_functions(self):
         self.button1_func = [self.zoom_out, self.zoom_in]
@@ -57,6 +63,22 @@ class GameLayout(FloatLayout):
         self.buttons.append(button2)
         self.buttons.append(button3)
         self.buttons.append(button4)
+        return layout
+
+    def get_button_hider(self):
+        layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
+        im1 = Image(source='{}\\button1.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+                    color=[0.8, 0.8, 0.8, 1])
+        im2 = Image(source='{}\\button2.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+                    color=[0.8, 0.8, 0.8, 1])
+        im3 = Image(source='{}\\button3.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+                    color=[0.8, 0.8, 0.8, 1])
+        im4 = Image(source='{}\\button4.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+                    color=[0.8, 0.8, 0.8, 1])
+        layout.add_widget(im1)
+        layout.add_widget(im2)
+        layout.add_widget(im3)
+        layout.add_widget(im4)
         return layout
 
     def zoom_out(self):
@@ -95,15 +117,39 @@ class GameLayout(FloatLayout):
         else:
             self.kivy_graph_in.kivy_graph.centralize_closest_neighbor_diff_color()
 
+    def call_function(self):
+        print "in 'call function', next func is:", self.next_func
+        if self.next_func is not None:
+            self.next_func()
+            self.tablet_game.send_info_from_screen()
+            self.move_counter += 1
+            self.next_func = None
+            if self.move_counter == self.tablet_game.max_turns:
+                self.remove_widget(self.button_layout)
+                self.remove_widget(self.button_hider)
+                end_layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
+                end_button = TextButton('end game', self.end_game)
+                end_layout.add_widget(end_button)
+                self.add_widget(end_layout)
+                end_layout.pos = (0, 0)
+        else:
+            self.enable_buttons()
+
+    def disable_buttons(self):
+        self.add_widget(self.button_hider)
+        for button in self.buttons:
+                button.disabled = True
+
+    def enable_buttons(self):
+        for button in self.buttons:
+            button.disabled = False
+        self.remove_widget(self.button_hider)
+
     def set_button_status(self, status):
         for item in self.buttons:
             item.disabled = status
 
     def end_game(self):
-        self.remove_widget(self.button_layout)
-        end_layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
-        end_button = UniButton('end game', self.tablet_game.end_game)
-        end_layout.add_widget(end_button)
-        self.add_widget(end_layout)
-        end_layout.pos=(0,0)
+        self.rt.stop()
+        self.tablet_game.end_game()
 
--- kivyFiles/GraphButton.py
+++ kivyFiles/GraphButton.py
@@ -1,9 +1,10 @@
 from kivy.uix.button import Button
+from time import sleep
 
 
-class UniButton(Button):
-    def __init__(self, text, func, **kwargs):
-        super(UniButton, self).__init__(**kwargs)
+class TextButton(Button):
+    def __init__(self, text, func,**kwargs):
+        super(TextButton, self).__init__(**kwargs)
         self.text = text
         self.on_press = func
 
@@ -13,29 +14,34 @@ class MultiButton(Button):
     counter = 0
     moves = 10
 
-    def __init__(self, img, funcs, button_num, game_layout = None, **kwargs):
+    def __init__(self, img, funcs, button_num, game_layout=None, **kwargs):
         super(MultiButton, self).__init__(size_hint_x=None, width=game_layout.button_width, **kwargs)
         self.game_layout = game_layout
         self.background_normal = img
         self.funcs = funcs
-        self.on_press = self.get_func
+        #self.on_press = self.get_func
         self.num = button_num
         self.lst = self.game_layout.tablet_game.button_presses
         self.set_disabled = self.game_layout.set_button_status
         self.moves = self.game_layout.tablet_game.max_turns
         self.send_data = game_layout.tablet_game.send_info_from_screen
 
-    def get_func(self):
-        print len(self.lst)
-        self.set_disabled(True)
-        if len(self.lst) >= self.moves:
-            self.game_layout.tablet_game.end_game()
-        else:
+    def on_press1(self):
+        # self.game_layout.set_button_status(True)
+        self.disabled = True
+        f = self.counter % len(self.funcs)
+        self.funcs[f]()
+        self.counter += 1
+        self.lst.append(self.num)
+        self.send_data()
+        if self.game_layout.move_counter == self.moves:
+            self.game_layout.end_game()
+        self.disabled = False
+
+    def on_press(self):
+        if self.game_layout.next_func is None:
+            self.game_layout.disable_buttons()
             f = self.counter % len(self.funcs)
-            self.funcs[f]()
+            self.game_layout.next_func = self.funcs[f]
             self.counter += 1
             self.lst.append(self.num)
-            self.send_data()
-            if len(self.lst) == self.moves:
-                self.game_layout.end_game()
-            self.set_disabled(False)
