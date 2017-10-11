4c222270584bdc9b9d19e41efe7b5d8201b61dcf
Omer-SS <omershakked@gmnail.com>
2017-10-11 10:42:38 +0300

    Fixed issue inKivyRuuner

--- kivyFiles/KivyRunner.py
+++ kivyFiles/KivyRunner.py
@@ -6,7 +6,7 @@ from SupplementaryFiles.LoadGraph import load_graph_from_file
 from KivyGraphTester import *
 from GraphGeneration import HandmadeGraph
 
-graph_file_path = "../GraphsData/draft_graph2.xml"
+graph_file_path = "../GraphsData/Graph_1.xml"
 graph_data = HandmadeGraph.create_draft_graph_2()
 
 
@@ -16,18 +16,18 @@ class GameType(Enum):
 
 
 def main(type, graph_data):
-    if game_type == GameType.VIEW_ONLY:
-        game = DisplayApp(graph)
+    if type == GameType.VIEW_ONLY:
+        game = DisplayApp(graph_data)
     elif game_type == GameType.ALLOW_PLAY:
         button_presses = []
         # This needs to be more versatile
-        game = GraphGameApp(TestScreen(graph, button_presses, 0.2))
+        game = GraphGameApp(TestScreen(graph_data, button_presses, 0.2))
     game.run()
 
 
 if __name__ == "__main__":
     game_type = GameType.VIEW_ONLY
-    
+
     if game_type == GameType.VIEW_ONLY:
         graph = load_graph_from_file(graph_file_path)
     else:
