acb3bbc7f44ea5c6a3fd0aad1b5fc911316d1ef2
Omer-SS <omershakked@gmnail.com>
2017-10-12 18:48:27 +0300

    WIP graph -4 needs checking

--- GraphGeneration/HandmadeGraph.py
+++ GraphGeneration/HandmadeGraph.py
@@ -157,14 +157,20 @@ def create_draft_graph_4():
     draft_graph.add_node(x_loc=390, y_loc=920, node_colour=Colours['red'], serial="n1")
     draft_graph.add_node(x_loc=200, y_loc=1100, node_colour=Colours['yellow'], serial="n2")
     draft_graph.add_node(x_loc=40, y_loc=500, node_colour=Colours['blue'], serial="n3")
-    draft_graph.add_node(x_loc=690, y_loc=1100, node_colour=Colours['red'], serial="n4")
+    draft_graph.add_node(x_loc=695, y_loc=1105, node_colour=Colours['red'], serial="n4")
     draft_graph.add_node(x_loc=880, y_loc=1000, node_colour=Colours['yellow'], serial="n5")
-    draft_graph.add_node(x_loc=1220, y_loc=1000, node_colour=Colours['yellow'], serial="n6")
-    draft_graph.add_node(x_loc=1000, y_loc=1300, node_colour=Colours['blue'], serial="n7")
-    draft_graph.add_node(x_loc=1400, y_loc=700, node_colour=Colours['red'], serial="n8")
-    draft_graph.add_node(x_loc=700, y_loc=800, node_colour=Colours['blue'], serial="n9")
+    draft_graph.add_node(x_loc=1240, y_loc=1005, node_colour=Colours['yellow'], serial="n6")
+    draft_graph.add_node(x_loc=997, y_loc=1297, node_colour=Colours['blue'], serial="n7")
+    draft_graph.add_node(x_loc=1400, y_loc=700, node_colour=Colours['blue'], serial="n8")
+    draft_graph.add_node(x_loc=745, y_loc=800, node_colour=Colours['blue'], serial="n9")
+    draft_graph.add_node(x_loc=1850, y_loc=705, node_colour=Colours['blue'], serial="n10")
+    draft_graph.add_node(x_loc=1500, y_loc=955, node_colour=Colours['yellow'], serial="n11")
+    draft_graph.add_node(x_loc=1950, y_loc=900, node_colour=Colours['red'], serial="n12")
+    draft_graph.add_node(x_loc=1000, y_loc=300, node_colour=Colours['red'], serial="n13")
+    draft_graph.add_node(x_loc=1507, y_loc=312, node_colour=Colours['red'], serial="n14")
+    draft_graph.add_node(x_loc=1405, y_loc=13010, node_colour=Colours['red'], serial="n15")
 
-    draft_graph.center_node = "n1"
+    draft_graph.center_node = "n14"
 
     for node in draft_graph.node_list:
         draft_graph.get_possible_connections(node.serial_num)
@@ -178,7 +184,14 @@ def create_draft_graph_4():
     draft_graph.connect_nodes(draft_graph.get_node_by_serial("n5"), draft_graph.get_node_by_serial("n9"))
     draft_graph.connect_nodes(draft_graph.get_node_by_serial("n5"), draft_graph.get_node_by_serial("n7"))
     draft_graph.connect_nodes(draft_graph.get_node_by_serial("n6"), draft_graph.get_node_by_serial("n8"))
-
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n6"), draft_graph.get_node_by_serial("n11"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n7"), draft_graph.get_node_by_serial("n15"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n8"), draft_graph.get_node_by_serial("n9"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n8"), draft_graph.get_node_by_serial("n10"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n8"), draft_graph.get_node_by_serial("n13"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n8"), draft_graph.get_node_by_serial("n14"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n10"), draft_graph.get_node_by_serial("n12"))
+    draft_graph.connect_nodes(draft_graph.get_node_by_serial("n10"), draft_graph.get_node_by_serial("n14"))
 
     save_graph(draft_graph, "draft_graph_4.xml")
 
