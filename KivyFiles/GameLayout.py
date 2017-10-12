33002b783c601a7ca3040d8acde40ca5f6a6863e
talkami <talkaminker@yahoo.com>
2017-10-12 11:07:00 +0300

    change in the way buttons' images are loaded and added comments

--- KivyFiles/GameLayout.py
+++ KivyFiles/GameLayout.py
@@ -3,11 +3,12 @@ kivy.require('1.9.1')
 
 from SupplementaryFiles.Utils import *
 from kivy.uix.gridlayout import GridLayout
-from GraphButton import MultiButton, TextButton
+from GraphButton import MultiButton, EndButton
 from GraphLayout import GraphLayout
 from kivy.uix.floatlayout import FloatLayout
 from SupplementaryFiles.RepeatedTimer import RepeatedTimer
 from kivy.uix.image import Image
+from os import path
 
 
 class GameLayout(FloatLayout):
@@ -38,6 +39,9 @@ class GameLayout(FloatLayout):
         self.button_hider = self.get_button_hider()
 
     def set_button_functions(self):
+        '''
+        This function is used in order to determine the functionality of the buttons
+        '''
         self.button1_func = [self.zoom_out, self.zoom_in]
         self.button2_func = [self.centralize_most_connected]
         self.button3_func = [self.centralize_closest_same_color]
@@ -45,15 +49,19 @@ class GameLayout(FloatLayout):
 
     def get_buttons(self):
         """
-        creates a GridLayout that would hold the buttons (GraphButtons) needed for the game. each button should be
-        initialized using a string representing an image to be displayed on the button and a function that will be
-        responsible for the button's functionality
+        Creates a GridLayout that holds GraphButton needed for the game. Each button should be
+        initialized using a string representing an image to be displayed on the button, a list of functions that will be
+        responsible for the button's functionality, the button's number and the current gameLayout (self).
         """
         layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
-        button1 = MultiButton('{}\\button1.jpg'.format(Utils.image_folder), self.button1_func, 1, self)
-        button2 = MultiButton('{}\\button2.jpg'.format(Utils.image_folder), self.button2_func, 2, self)
-        button3 = MultiButton('{}\\button3.jpg'.format(Utils.image_folder), self.button3_func, 3, self)
-        button4 = MultiButton('{}\\button4.jpg'.format(Utils.image_folder), self.button4_func, 4, self)
+        button1 = MultiButton(img=path.join(Utils.image_folder, Utils.btn_1_img), funcs=self.button1_func,
+                              button_num=1, game_layout=self)
+        button2 = MultiButton(img=path.join(Utils.image_folder, Utils.btn_2_img), funcs=self.button2_func,
+                              button_num=2, game_layout=self)
+        button3 = MultiButton(img=path.join(Utils.image_folder, Utils.btn_3_img), funcs=self.button3_func,
+                              button_num=3, game_layout=self)
+        button4 = MultiButton(img=path.join(Utils.image_folder, Utils.btn_4_img), funcs=self.button4_func,
+                              button_num=4, game_layout=self)
         layout.add_widget(button1)
         layout.add_widget(button2)
         layout.add_widget(button3)
@@ -66,14 +74,18 @@ class GameLayout(FloatLayout):
         return layout
 
     def get_button_hider(self):
+        '''
+        Creates a GridLayout that looks similar to the button layout but is invisible. This is used in order to
+        'disable' the buttons after pressing (disallowing double press)
+        '''
         layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
-        im1 = Image(source='{}\\button1.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+        im1 = Image(source=path.join(Utils.image_folder, Utils.btn_1_img), allow_stretch=True, keep_ratio=False,
                     color=[0.8, 0.8, 0.8, 1])
-        im2 = Image(source='{}\\button2.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+        im2 = Image(source=path.join(Utils.image_folder, Utils.btn_2_img), allow_stretch=True, keep_ratio=False,
                     color=[0.8, 0.8, 0.8, 1])
-        im3 = Image(source='{}\\button3.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+        im3 = Image(source=path.join(Utils.image_folder, Utils.btn_3_img), allow_stretch=True, keep_ratio=False,
                     color=[0.8, 0.8, 0.8, 1])
-        im4 = Image(source='{}\\button4.jpg'.format(Utils.image_folder), allow_stretch=True, keep_ratio=False,
+        im4 = Image(source=path.join(Utils.image_folder, Utils.btn_4_img), allow_stretch=True, keep_ratio=False,
                     color=[0.8, 0.8, 0.8, 1])
         layout.add_widget(im1)
         layout.add_widget(im2)
@@ -82,6 +94,55 @@ class GameLayout(FloatLayout):
         layout.pos = (0, 0)
         return layout
 
+    def call_function(self):
+        '''
+        Function checks if any button put a new argument in 'next_func' variable, if so said argument is called and
+         variable is reset, otherwise all buttons are enabled
+        '''
+        if self.next_func is not None:
+            self.next_func()
+            self.tablet_game.send_info_from_screen()
+            self.move_counter += 1
+            self.next_func = None
+            if self.move_counter == self.tablet_game.max_turns:
+                # once all the moves are played, removes the buttons from the screen and creates a new button in order
+                # to finish the game
+                self.remove_widget(self.button_layout)
+                self.remove_widget(self.button_hider)
+                end_layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
+                end_button = EndButton(self.end_game)
+                end_layout.add_widget(end_button)
+                self.add_widget(end_layout)
+                end_layout.pos = (0, 0)
+        else:
+            self.enable_buttons()
+
+    def disable_buttons(self):
+        '''
+        function renders the buttons unpressable
+        '''
+        self.add_widget(self.button_hider)
+        for button in self.buttons:
+                button.disabled = True
+
+    def enable_buttons(self):
+        '''
+        function renders the buttons pressable
+        '''
+        for button in self.buttons:
+            button.disabled = False
+        self.remove_widget(self.button_hider)
+
+    def end_game(self):
+        '''
+        function should be called after all moves are done. finished current graph diaplay inorder to move to the
+        question screen
+        '''
+        self.rt.stop()
+        self.tablet_game.end_game()
+
+    # The following functions represent button functionality. Zoom in/out switched between the GraphLayouts, other
+    # functions identified the current GraphLayout and calls its corresponding function
     def zoom_out(self):
         center_node = self.kivy_graph_out.kivy_graph.get_by_serial(self.kivy_graph_in.kivy_graph.center_node.serial)
         self.kivy_graph_out.kivy_graph.move_node_to_center(center_node, False)
@@ -121,35 +182,3 @@ class GameLayout(FloatLayout):
             self.kivy_graph_out.kivy_graph.centralize_closest_neighbor_diff_color()
         else:
             self.kivy_graph_in.kivy_graph.centralize_closest_neighbor_diff_color()
-
-    def call_function(self):
-        if self.next_func is not None:
-            self.next_func()
-            self.tablet_game.send_info_from_screen()
-            self.move_counter += 1
-            self.next_func = None
-            if self.move_counter == self.tablet_game.max_turns:
-                self.remove_widget(self.button_layout)
-                self.remove_widget(self.button_hider)
-                end_layout = GridLayout(cols=1, col_default_width=self.button_width, col_force_default=True)
-                end_button = TextButton('end game', self.end_game)
-                end_layout.add_widget(end_button)
-                self.add_widget(end_layout)
-                end_layout.pos = (0, 0)
-        else:
-            self.enable_buttons()
-
-    def disable_buttons(self):
-        self.add_widget(self.button_hider)
-        for button in self.buttons:
-                button.disabled = True
-
-    def enable_buttons(self):
-        for button in self.buttons:
-            button.disabled = False
-        self.remove_widget(self.button_hider)
-
-    def end_game(self):
-        self.rt.stop()
-        self.tablet_game.end_game()
-
