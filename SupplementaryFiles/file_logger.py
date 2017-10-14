66ef4655a42ce51aca9e7737481ac6feab3c87a1
nogur9 <nogur9@gmail.com>
2017-10-14 14:59:45 +0300

    GLogger

--- SupplementaryFiles/file_logger.py
+++ SupplementaryFiles/file_logger.py
@@ -4,3 +4,9 @@ class file_logger ():
 
     def __init__(self, writing_location):
         self.path = writing_location
+        open(self.path, 'w').close()
+
+    def log_write (self,msg):
+        log_file = open(self.path, 'a')
+        log_file.write("{}\n".format(msg))
+        log_file.close()
