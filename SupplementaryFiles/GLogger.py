21e3ac093095e436636d3e02d07e0371db4aad08
nogur9 <nogur9@gmail.com>
2017-10-14 19:16:51 +0300

    GLogger

--- SupplementaryFiles/GLogger.py
+++ SupplementaryFiles/GLogger.py
@@ -1,6 +1,5 @@
 import logging
-from SupplementaryFiles.file_logger import file_logger
-from SupplementaryFiles.server_logger import server_logger
+
 class GLogger ():
     logger = None
     log_output_type = ""
@@ -62,3 +61,42 @@ class LogAction:
     def __init__(self):
         pass
 
+class server_logger ():
+    ip = ""
+
+    def __init__(self, writing_location, user_data_dir):
+        self.ip = writing_location
+        self.init_communication(self.ip,user_data_dir)
+
+    def log_write (self,**kwargs):
+        from KivyCommunication import *
+        KL.log.insert(**kwargs)
+
+
+    def init_communication(self, server_ip, user_data_dir):
+        from KivyCommunication import *
+        """
+        Initiolize the communication protocol to the server.
+        """
+        KC.start(the_ip=server_ip, the_parents=[self])
+        KL.start(mode=[DataMode.file], pathname=user_data_dir)
+        KL.restart()
+
+    #
+    # @staticmethod
+    # def on_connection():
+    #     pass
+
+
+
+class file_logger ():
+    path = ""
+
+    def __init__(self, writing_location):
+        self.path = writing_location
+        open(self.path, 'w').close()
+
+    def log_write (self,msg):
+        log_file = open(self.path, 'a')
+        log_file.write("{}\n".format(msg))
+        log_file.close()
